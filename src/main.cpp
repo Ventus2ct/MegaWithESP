ef4f04d4e14bcf76585c38edfd7308938c0c95ac
Ventus2ct <lars.rune.bjornevik@gmail.com>
2023-02-05 21:30:22 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,6 @@ void setup() {
   
   Serial.println("Staring");
   espm.SetSSID("aSid-Net","saab_9-5");
-  //espm.SetSSID("LRB-iPhone","Canon-01");
-  //espm.SetSSID("bamsebo","system-B2");
   //                       12345678 MUST have 8 characters 
   //espm.setupNet();
   espm.setupAPStation();
